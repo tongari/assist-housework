1dc58b0880658d8261c5e863d7507e696a56a59e
tongari <info@tongari-as.com>
2020-08-10 20:12:08 +0900

    Create firebase login

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,54 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import * as firebase from 'firebase/app'
+import * as firebaseui from 'firebaseui'
+import 'firebaseui/dist/firebaseui.css'
+// import React from 'react'
+// import ReactDOM from 'react-dom'
+// import App from 'components/App'
 import './index.css'
-import App from 'components/App'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-)
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_CONFIG_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_CONFIG_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_CONFIG_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_CONFIG_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_CONFIG_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_CONFIG_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_CONFIG_APP_ID,
+}
+
+firebase.initializeApp(firebaseConfig)
+
+const ui = new firebaseui.auth.AuthUI(firebase.auth())
+
+const uiConfig = {
+  callbacks: {
+    signInSuccessWithAuthResult: () => {
+      return true
+    },
+    uiShown: () => {
+      const loaderEl = document.getElementById('loader')
+      if (loaderEl) {
+        loaderEl.style.display = 'none'
+      }
+    },
+  },
+  // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
+  signInFlow: 'popup',
+  signInSuccessUrl: '/',
+  signInOptions: [
+    {
+      provider: firebase.auth.EmailAuthProvider.PROVIDER_ID,
+      signInMethod: firebase.auth.EmailAuthProvider.EMAIL_LINK_SIGN_IN_METHOD,
+    },
+    firebase.auth.PhoneAuthProvider.PROVIDER_ID,
+  ],
+}
+
+ui.start('#firebaseui-auth-container', uiConfig)
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// )
