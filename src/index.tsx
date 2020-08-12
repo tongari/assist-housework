d1cfb170e8bc7fb7cdd0efaab05b68fda390c5e7
tongari <info@tongari-as.com>
2020-08-12 10:18:03 +0900

    Refactoring

--- src/index.tsx
+++ src/index.tsx
@@ -1,21 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import * as firebase from 'firebase/app'
-
+import initializeFirebase from 'settings/firebase'
 import App from './App'
 import './index.css'
 
-const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_CONFIG_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_CONFIG_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_FIREBASE_CONFIG_DATABASE_URL,
-  projectId: process.env.REACT_APP_FIREBASE_CONFIG_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_CONFIG_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_CONFIG_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_CONFIG_APP_ID,
-}
-
-firebase.initializeApp(firebaseConfig)
+initializeFirebase()
 
 ReactDOM.render(
   <React.StrictMode>
---
+++ src/settings/firebase.ts
@@ -0,0 +1,17 @@
+import * as firebase from 'firebase/app'
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_CONFIG_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_CONFIG_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_CONFIG_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_CONFIG_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_CONFIG_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_CONFIG_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_CONFIG_APP_ID,
+}
+
+const initializeFirebase = (): void => {
+  firebase.initializeApp(firebaseConfig)
+}
+
+export default initializeFirebase
