5311ccd7517079f61400df638607f76c46489356
tongari <tongari@users.noreply.github.com>
2020-08-13 10:46:32 +0900

    Pr: Create register approver page (#3)
    * Create useAuthState hook
    
    * Refactoring
    
    * Create register approver page
    
    * Use react-firebase-hooks
    
    * Refactoring

---
+++ src/hooks/auth/useLogin.tsx
@@ -0,0 +1,46 @@
+import { useEffect } from 'react'
+import * as firebase from 'firebase/app'
+import * as firebaseui from 'firebaseui'
+import 'firebaseui/dist/firebaseui.css'
+import { useAuthState } from 'react-firebase-hooks/auth'
+
+const uiConfig = {
+  callbacks: {
+    signInSuccessWithAuthResult: () => false,
+  },
+  signInFlow: 'popup',
+  signInOptions: [
+    {
+      provider: firebase.auth.EmailAuthProvider.PROVIDER_ID,
+      signInMethod: firebase.auth.EmailAuthProvider.EMAIL_LINK_SIGN_IN_METHOD,
+    },
+    firebase.auth.PhoneAuthProvider.PROVIDER_ID,
+  ],
+}
+
+const useLogin = (): [
+  firebase.User | undefined,
+  boolean,
+  firebase.auth.Error | undefined
+] => {
+  const [user, isLoading, error] = useAuthState(firebase.auth())
+
+  useEffect(() => {
+    const el = document.getElementById(
+      'firebaseui-auth-container'
+    ) as HTMLDivElement
+
+    if (isLoading || user) {
+      el.style.display = 'none'
+      return
+    }
+
+    el.style.display = 'block'
+    const ui = new firebaseui.auth.AuthUI(firebase.auth())
+    ui.start('#firebaseui-auth-container', uiConfig)
+  }, [user, isLoading])
+
+  return [user, isLoading, error]
+}
+
+export default useLogin
