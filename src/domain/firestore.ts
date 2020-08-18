a1ec2c2b18e75082bb3cd868c4b5c8170ba301b3
tongari <info@tongari-as.com>
2020-08-18 13:42:25 +0900

    Practice firestore

--- src/domain/firestore.ts
+++ src/domain/firestore.ts
@@ -2,13 +2,15 @@ import * as firebase from 'firebase/app'
 
 // TODO: ちょっと模索中
 
-type TestSaveResultType = firebase.firestore.DocumentReference<
+type CreateTestResultType = firebase.firestore.DocumentReference<
   firebase.firestore.DocumentData
 >
 // eslint-disable-next-line import/prefer-default-export
-export const testSave = async (cnt: number): Promise<TestSaveResultType> => {
+export const createTest = async (
+  cnt: number
+): Promise<CreateTestResultType> => {
   const db = firebase.firestore()
-  const result = await db.collection('test').add({
+  const result = await db.collection('tests').add({
     cnt,
     created_at: firebase.firestore.FieldValue.serverTimestamp(),
     updated_at: firebase.firestore.FieldValue.serverTimestamp(),
