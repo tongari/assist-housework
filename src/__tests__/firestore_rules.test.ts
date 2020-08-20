bb1187ec79b4ff6e17193ea9ed612ed721c974d9
tongari <info@tongari-as.com>
2020-08-20 13:56:47 +0900

    Update users collection rules

--- src/__tests__/firestore_rules.test.ts
+++ src/__tests__/firestore_rules.test.ts
@@ -16,6 +16,50 @@ describe('firestore rules', () => {
     })
   })
 
+  beforeEach(async () => {
+    const admin = firebase
+      .initializeAdminApp({ projectId: PROJECT_ID })
+      .firestore()
+    await admin.collection('roles').doc('1').set({
+      role: 'admin',
+    })
+    await admin.collection('roles').doc('2').set({
+      role: 'approver',
+    })
+    await admin.collection('roles').doc('3').set({
+      role: 'assistant',
+    })
+
+    const rolesRef = admin.collection('roles')
+    await admin
+      .collection('users')
+      .doc('approver_1')
+      .set({
+        userId: 'approver_1',
+        nickName: 'approver_1 san',
+        assistantUserIds: ['assistant_1', 'assistant_2'],
+        roleRef: rolesRef.doc('2'),
+      })
+    await admin
+      .collection('users')
+      .doc('approver_2')
+      .set({
+        userId: 'approver_2',
+        nickName: 'approver_2 san',
+        assistantUserIds: ['assistant_11', 'assistant_22'],
+        roleRef: rolesRef.doc('2'),
+      })
+    await admin
+      .collection('users')
+      .doc('assistant_1')
+      .set({
+        userId: 'assistant_1',
+        nickName: 'assistant_1 san',
+        assistantUserIds: [],
+        roleRef: rolesRef.doc('3'),
+      })
+  })
+
   afterEach(async () => {
     await firebase.clearFirestoreData({ projectId: PROJECT_ID })
   })
@@ -27,15 +71,60 @@ describe('firestore rules', () => {
   describe('roles collection tests', () => {
     describe('read', () => {
       test('Authorized user can read role', async () => {
-        const db = authorizedApp({ uid: 'hoge' })
-        const adminRole = db.collection('roles').doc()
-        await firebase.assertSucceeds(adminRole.get())
+        const db = authorizedApp({ uid: 'approver_1' })
+        const rolesRef = db.collection('roles')
+        await firebase.assertSucceeds(rolesRef.get())
       })
 
       test("Unauthorized user can't read role", async () => {
         const db = authorizedApp()
-        const adminRole = db.collection('roles').doc()
-        await firebase.assertFails(adminRole.get())
+        const rolesRef = db.collection('roles')
+        await firebase.assertFails(rolesRef.get())
+      })
+    })
+
+    describe('write', () => {
+      test("can't create role", async () => {
+        const db = authorizedApp({ uid: 'approver_1' })
+        await firebase.assertFails(
+          db.collection('roles').doc('4').set({
+            role: 'anonymous',
+          })
+        )
+      })
+    })
+  })
+
+  describe('users collection tests', () => {
+    describe('read', () => {
+      test('My account can read', async () => {
+        const db = authorizedApp({ uid: 'approver_1' })
+        const approverUser = db.collection('users').doc('approver_1')
+        await firebase.assertSucceeds(approverUser.get())
+      })
+
+      test('Approval user can read my assistant', async () => {
+        const db = authorizedApp({ uid: 'approver_1' })
+        const assistantUser = db.collection('users').doc('assistant_1')
+        await firebase.assertSucceeds(assistantUser.get())
+      })
+
+      test('Approval user can not read other assistant', async () => {
+        const db = authorizedApp({ uid: 'approver_2' })
+        const assistantUser = db.collection('users').doc('assistant_1')
+        await firebase.assertFails(assistantUser.get())
+      })
+
+      test('Assistant user can not read approver', async () => {
+        const db = authorizedApp({ uid: 'assistant_1' })
+        const approverUser = db.collection('users').doc('approver_1')
+        await firebase.assertFails(approverUser.get())
+      })
+
+      test('Unauthorized user can not read assistant', async () => {
+        const db = authorizedApp()
+        const assistantUser = db.collection('users').doc('assistant_1')
+        await firebase.assertFails(assistantUser.get())
       })
     })
   })
