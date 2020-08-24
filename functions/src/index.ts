204ecf9652a6657a1d5c38118e7081bf2bb0ce29
tongari <tongari@users.noreply.github.com>
2020-08-24 15:40:15 +0900

    Pr: Set firebase emulators(Stil not woriking on docker) (#11)
    * Test firebase emulators
    
    * Fix firebase emulators(Stil not woriking on docker)

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,10 +1,27 @@
 import * as functions from 'firebase-functions'
+import * as admin from 'firebase-admin'
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// eslint-disable-next-line import/prefer-default-export
-export const helloWorld = functions.https.onRequest((request, response) => {
+admin.initializeApp()
+
+export const helloWorld = functions.https.onRequest((_, response) => {
   functions.logger.info('Hello logs!', { structuredData: true })
   response.send('Hello from Firebase!')
 })
+
+export const addMessage = functions.https.onRequest(async (req, res) => {
+  const original = req.query.text
+  const writeResult = await admin
+    .firestore()
+    .collection('messages')
+    .add({ original })
+  res.json({ result: `Message with ID: ${writeResult.id} added.` })
+})
+
+exports.makeUppercase = functions.firestore
+  .document('/messages/{documentId}')
+  .onCreate((snap, context) => {
+    const originalData = snap.data().original
+    functions.logger.log('Uppercasing', context.params.documentId, originalData)
+    const uppercase = originalData.toUpperCase()
+    return snap.ref.set({ uppercase }, { merge: true })
+  })
