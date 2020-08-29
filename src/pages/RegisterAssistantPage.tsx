7fb823d420fcddce319cb4bee15b3e507bd04e0b
tongari <info@tongari-as.com>
2020-08-29 18:57:39 +0900

    Create pengind assistant page

--- src/pages/RegisterAssistantPage.tsx
+++ src/pages/RegisterAssistantPage.tsx
@@ -1,12 +1,28 @@
 import React from 'react'
+import * as firebase from 'firebase/app'
 import { registerAssistantUser } from 'domain/firestore'
+import { useDocument } from 'react-firebase-hooks/firestore'
 import RegisterAssistant from 'components/templates/RegisterAssistant'
+import PendingRegisterAssistant from 'components/templates/RegisterAssistant/pending'
 
 const RegisterAssistantPage: React.FC = () => {
   const searchParams = new URLSearchParams(window.location.search)
+  const assistToApproverId = searchParams.get('invite_assistant')
+
+  const [assistToApprovers] = useDocument(
+    firebase
+      .firestore()
+      .doc(
+        `users/${
+          firebase.auth().currentUser?.uid
+        }/assistToApprovers/${assistToApproverId}`
+      ),
+    {
+      snapshotListenOptions: { includeMetadataChanges: true },
+    }
+  )
 
   const registerAssistantUserHandler = (nickName: string) => {
-    const assistToApproverId = searchParams.get('invite_assistant')
     registerAssistantUser(nickName, assistToApproverId).catch((err) => {
       // eslint-disable-next-line no-alert
       window.alert(err.message)
@@ -15,10 +31,16 @@ const RegisterAssistantPage: React.FC = () => {
 
   return (
     <>
-      <RegisterAssistant
-        approverNickName={searchParams.get('approver_nick_name') ?? ''}
-        registerAssistantUserHandler={registerAssistantUserHandler}
-      />
+      {assistToApprovers?.get('assistToApproverId') ? (
+        <PendingRegisterAssistant
+          approverNickName={searchParams.get('approver_nick_name') ?? ''}
+        />
+      ) : (
+        <RegisterAssistant
+          approverNickName={searchParams.get('approver_nick_name') ?? ''}
+          registerAssistantUserHandler={registerAssistantUserHandler}
+        />
+      )}
     </>
   )
 }
